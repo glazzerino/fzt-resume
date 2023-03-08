59a2c8af1a122050dd87ec397560166b60f9a8cf
glazzerino <fztmon@gmail.com>
2023-03-07 21:44:42 -0600

    Completed the thing

---
+++ src/app/workitem/workitem.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkitemComponent } from './workitem.component';
+
+describe('WorkitemComponent', () => {
+  let component: WorkitemComponent;
+  let fixture: ComponentFixture<WorkitemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WorkitemComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WorkitemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
