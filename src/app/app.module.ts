219c92f3714d7fd07c0bae14b731ef3a1063bd06
glazzerino <fztmon@gmail.com>
2023-03-07 21:44:32 -0600

    Completed the thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WorkitemComponent } from './workitem/workitem.component';
+import { WorkitemParentComponent } from './workitem-parent/workitem-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WorkitemComponent,
+    WorkitemParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
